e2a5773a1bb309536c3d69fb15bde9dcbcd2449b
Pestana Pedro Catumbela <pestannapedrocatumbella@gmail.com>
2024-02-23 19:03:26 -0300

    Feat: Add Models

---
+++ Models/Todo.cs
@@ -0,0 +1,11 @@
+namespace TW_ToDo.Models;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateOnly Deadline { get; set; }
+    public DateOnly FinishedAt { get; set; }
+}
